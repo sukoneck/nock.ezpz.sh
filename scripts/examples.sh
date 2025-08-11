1ed87d4883a8fc476b7c5487e2122c6392a1149e
Daniel Sukoneck <daniel@sukoneck.com>
2025-08-11 03:31:35 +0100

    add docs

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,6 +1,22 @@
-curl "https://api.nock.ezpz.sh/ls?prefix=pub/"
-
-curl -fSLo gm.log "https://api.nock.ezpz.sh/pub/gm.log"
-
 # Hello world
-curl -fsSL "https://api.nock.ezpz.sh/pub/gm"
+curl -sS "https://api.nock.ezpz.sh/pub/gm"
+
+# List public files
+curl -sS "https://api.nock.ezpz.sh/ls?prefix=pub/" | jq .
+
+# Download a public file
+curl -o latest.jam "https://api.nock.ezpz.sh/pub/file.txt"
+
+# Download a private file (requires authentication)
+curl -o file.txt "https://api.nock.ezpz.sh/priv/dir/file.txt"
+
+# Upload a private file (requires authentication)
+curl -sST file.txt "https://api.nock.ezpz.sh/priv/writable/file.txt"
+
+# Check permissions
+curl -sS "https://api.nock.ezpz.sh/auth/verify" | jq .
+
+# Endpoints that require authentication may use either:
+  -H "Authorization: Bearer YOUR_TOKEN"
+# or
+  ?key=YOUR_TOKEN
